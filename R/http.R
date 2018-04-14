b49220247fc7a40b622f535905ab81612cf1595e
Thomas J. Leeper <thosjleeper@gmail.com>
2018-04-14 17:15:06 +0100

    cleanup http code & fix session_token arg (closes #5)

--- R/http.R
+++ R/http.R
@@ -1,24 +1,39 @@
 #' @title SES HTTP Requests
 #' @description Low-level SES POST function
 #' @param query A list containing query string parameters
+#' @param headers A list of headers to pass to the HTTP request.
 #' @param body A list of query-like parameters to be passed as a form-encoded message body.
-#' @param region A character string containing an AWS region. If missing, the default \dQuote{us-east-1} is used.
-#' @param key A character string containing an AWS Access Key ID. See \code{\link[aws.signature]{locate_credentials}}.
-#' @param secret A character string containing an AWS Secret Access Key. See \code{\link[aws.signature]{locate_credentials}}.
-#' @param session_token Optionally, a character string containing an AWS temporary Session Token. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param verbose A logical indicating whether to be verbose. Default is given by \code{options("verbose")}.
+#' @param region A character string specifying an AWS region. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param key A character string specifying an AWS Access Key. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param secret A character string specifying an AWS Secret Key. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param session_token Optionally, a character string specifying an AWS temporary Session Token to use in signing a request. See \code{\link[aws.signature]{locate_credentials}}.
 #' @param \dots Additional arguments passed to \code{\link[httr]{POST}}.
 #' @import httr
 #' @importFrom aws.signature signature_v4_auth
 #' @importFrom utils URLencode
 #' @importFrom jsonlite fromJSON
 #' @export
-sesPOST <- function(query = list(), 
-                    body = NULL,
-                    region = Sys.getenv("AWS_DEFAULT_REGION","us-east-1"), 
-                    key = NULL, 
-                    secret = NULL, 
-                    session_token = NULL,
-                    ...) {
+sesPOST <-
+function(
+  query = list(),
+  headers = list(),
+  body = NULL,
+  verbose = getOption("verbose", FALSE),
+  region = Sys.getenv("AWS_DEFAULT_REGION", "us-east-1"), 
+  key = NULL, 
+  secret = NULL, 
+  session_token = NULL,
+  ...
+) {
+    # locate and validate credentials
+    credentials <- locate_credentials(key = key, secret = secret, session_token = session_token, region = region, verbose = verbose)
+    key <- credentials[["key"]]
+    secret <- credentials[["secret"]]
+    session_token <- credentials[["session_token"]]
+    region <- credentials[["region"]]
+    
+    # generate request signature
     uri <- paste0("https://email.",region,".amazonaws.com")
     d_timestamp <- format(Sys.time(), "%Y%m%dT%H%M%SZ", tz = "UTC")
     body_to_sign <- if (is.null(body)) {
@@ -38,16 +53,18 @@ sesPOST <- function(query = list(),
            request_body = body_to_sign,
            key = key, 
            secret = secret,
-           session_token = session_token)
-    headers <- list()
+           session_token = session_token,
+           verbose = verbose)
+    # setup request headers
     headers[["x-amz-date"]] <- d_timestamp
     headers[["x-amz-content-sha256"]] <- Sig$BodyHash
+    headers[["Authorization"]] <- Sig[["SignatureHeader"]]
     if (!is.null(session_token) && session_token != "") {
         headers[["x-amz-security-token"]] <- session_token
     }
-    headers[["Authorization"]] <- Sig[["SignatureHeader"]]
     H <- do.call(add_headers, headers)
     
+    # execute request
     if (length(query)) {
         if (!is.null(body)) {
             r <- POST(uri, H, query = query, body = body, encode = "form", ...)
@@ -63,7 +80,7 @@ sesPOST <- function(query = list(),
     }
     
     if (http_error(r)) {
-        x <- try(fromJSON(content(r, "text", encoding = "UTF-8"))$Error, silent = TRUE)
+        x <- try(jsonlite::fromJSON(content(r, "text", encoding = "UTF-8"))$Error, silent = TRUE)
         warn_for_status(r)
         h <- headers(r)
         out <- structure(x, headers = h, class = "aws_error")
@@ -71,7 +88,7 @@ sesPOST <- function(query = list(),
         attr(out, "request_string_to_sign") <- Sig$StringToSign
         attr(out, "request_signature") <- Sig$SignatureHeader
     } else {
-        out <- try(fromJSON(content(r, "text", encoding = "UTF-8")), silent = TRUE)
+        out <- try(jsonlite::fromJSON(content(r, "text", encoding = "UTF-8")), silent = TRUE)
         if (inherits(out, "try-error")) {
             out <- structure(content(r, "text", encoding = "UTF-8"), class = "unknown")
         }
